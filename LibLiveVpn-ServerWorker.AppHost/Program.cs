8ef332f402becc9aaf0c7a9e7a85e261856ab3b5
libsyfer <libsyfer@gmail.com>
2025-01-02 18:41:03 +0300

    Создание основного проекта запуска присложения на замену предыдущему

---
+++ LibLiveVpn-ServerWorker.AppHost/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using LibLiveVpn_ServerWorker.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder();
+
+builder.ConfigureAppConfiguration((context, conf) =>
+{
+    conf.Sources.Clear();
+
+    conf.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    conf.AddJsonFile($"appsettings.{context.HostingEnvironment}.json", optional: true, reloadOnChange: true);
+    conf.AddEnvironmentVariables();
+    conf.AddUserSecrets(Assembly.GetExecutingAssembly());
+});
+
+builder.ConfigureServices((context, services) =>
+{
+    services.AddInfrastructureDependencies(context.Configuration);
+});
+
+var app = builder.Build();
+
+await Extensions.NotifyWorkerStart(app.Services);
+
+app.Run();
