2f5a103ec27652e64d6d3131975cf8d62d6cc17e
libsyfer <libsyfer@gmail.com>
2025-01-02 22:49:34 +0300

    Создание интерфейса сервиса управления vpn сервером

---
+++ LibLiveVpn-ServerWorker.Application/Interfaces/IVpnServerManager.cs
@@ -0,0 +1,15 @@
+﻿using LibLiveVpn_ServerWorker.Application.Models;
+
+namespace LibLiveVpn_ServerWorker.Application.Interfaces
+{
+    public interface IVpnServerManager
+    {
+        Task<CommandResult> CheckRequirementsAsync(CancellationToken cancellationToken);
+
+        Task<CommandResult> CreateAreaAsync(string areaName, string configuration, CancellationToken cancellationToken);
+
+        Task<CommandResult> UpdateAreaAsync(string areaName, string configuration, CancellationToken cancellationToken);
+
+        Task<CommandResult> DeleteAreaAsync(string areaName, CancellationToken cancellationToken);
+    }
+}
